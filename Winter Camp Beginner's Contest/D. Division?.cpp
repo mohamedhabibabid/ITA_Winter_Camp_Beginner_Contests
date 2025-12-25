e0833285a53021a856d4f6c93e2be8929dadeb3e
Mohamed Habib Abid <118135433+mohamedhabibabid@users.noreply.github.com>
2025-12-25 15:14:08 +0100

    Create D. Division?.cpp

---
+++ Winter Camp Beginner's Contest/D. Division?.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t; // number of test cases
+    cin>>t; 
+    
+    for(int i=0; i< t; i++){
+        int rate; 
+        cin>>rate; 
+        if( rate >= 1900){
+            cout<<"Division 1"<<endl;
+        }else if( rate >= 1600 && rate <= 1899){
+            cout<<"Division 2"<<endl;
+        }else if( rate >= 1400 && rate <= 1599){
+            cout<<"Division 3"<<endl;   
+        }else{
+            cout<<"Division 4"<<endl;
+        }
+    }
+
+    return 0;
+}
